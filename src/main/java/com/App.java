3f638c62329d4f616c26de51f499c8697599609c
vpeti <vpeti@LAPTOP-ISVALQPT>
2019-04-09 09:36:44 +0200

    App format

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,4 +1,5 @@
 package com;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -6,12 +7,12 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
-	
-	@Override
+
+    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(App.class);
+        return application.sources(App.class);
     }
-	
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
     }
